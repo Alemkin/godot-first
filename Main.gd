003e3d9f0f92b4ff28384be19589de65ea54ba0c
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-01-08 21:32:49 -0500

    Added music and death sounds to scene

--- Main.gd
+++ Main.gd
@@ -10,6 +10,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -17,6 +19,7 @@ func new_game():
 	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
